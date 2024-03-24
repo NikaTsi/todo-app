c65f0351ad8187d414100cd3299e91c4c6300528
NikaTsi <nikawiklauri54@gmail.com>
2024-03-24 08:18:29 +0400

    js done (fixed bugs)

--- main.js
+++ main.js
@@ -1,172 +1,128 @@
-const inpt = document.querySelector("input")
-const box = document.querySelector(".box")
-const specs = document.querySelector(".specs")
-const section = document.querySelector("section")
-const main = document.querySelector("main")
+const inpt = document.querySelector("input");
+const specs = document.querySelector(".specs");
+const section = document.querySelector("section");
+const main = document.querySelector("main");
+const footer = document.querySelector("footer")
 
-
-
-let todos = []
+let todos = [];
 
 inpt.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
-        updatePage()
-        display()
-        inpt.value = ""
+        updatePage();
+        inpt.value = "";
     }
-})
-
-
-
+});
 
 function updatePage() {
-    if (!inpt.value.trim() == "") {
-        
-        let task = {
-            title: inpt.value,
+    const inputValue = inpt.value.trim();
+    if (inputValue !== "") {
+        const task = {
+            title: inputValue,
             id: Math.random(),
-            // completed: false
-        }
-        todos.push(task)
-        main.classList.add("active")
-        updateDOM()
-        
+            completed: false
+        };
+        todos.push(task);
+        main.classList.add("active");
+        updateDOM();
+        display();
+        filterTasks(activeCategory);
     }
-    display()
 }
-
-
 
 function display() {
-        let count = todos.length
-        specs.innerHTML = `
-        <h3>${count} items left</h3>
-        <h3>Clear Tasks</h3>`
-    
-    if (todos.length == ""){
-        main.classList.remove("active")
-        
+    const count = todos.filter(task => !task.completed).length;
+    specs.innerHTML = `
+    <h3>${count} items left</h3>
+    <h3 onclick="clearCompleted()" class="clearAllTasks">Clear Completed</h3>`;
+
+    if (todos.length === 0) {
+        main.classList.remove("active");
+    } else {
+        main.classList.add("active");
     }
 }
 
-
 function deleteTask(taskId) {
-    todos = todos.filter(task => task.id !== taskId);
-    updateDOM()
-    display()
+    const task = todos.find(task => task.id === taskId);
+    if (task) {
+        todos = todos.filter(task => task.id !== taskId);
+        updateDOM();
+        display();
+        filterTasks(activeCategory);
+    }
 }
 
-
+function clearCompleted() {
+    todos = todos.filter(task => !task.completed);
+    updateDOM();
+    display();
+    filterTasks('all');
+}
 
 function updateDOM() {
-    section.innerHTML = ""; 
+    section.innerHTML = "";
     todos.forEach(task => {
-        let taskElement = document.createElement("div");
+        const taskElement = document.createElement("div");
         taskElement.classList.add("box");
+        taskElement.id = `${task.id}`
         taskElement.innerHTML = `
         <div class="task">
-        <div class="left">
-                    <img  onclick="completed(${task.id})" class="oval" src="./assets/mobile/oval.svg">
-                    <p id = "${task.id}">${task.title}</p>
-                </div>
-                <img onclick="deleteTask(${task.id})" class="delete" src="./assets/mobile/X.svg">
+            <div class="left">
+                <img onclick="completedTask(${task.id})" class="circle" src="${task.completed ? './assets/desktop/checked.svg' : './assets/mobile/circle.svg'}">
+                <p id="${task.id}" class="${task.completed ? 'completed' : ''}">${task.title}</p>
             </div>
-            <div class="line"></div>`;
+            <img onclick="deleteTask(${task.id})" class="delete" src="./assets/mobile/delete.svg">
+        </div>
+        <div class="line"></div>`;
         section.append(taskElement);
     });
 }
 
 
-function completed(taskId){
-    const p = document.getElementById(taskId)
-    p.classList.toggle("completed")
-
-    const oval = document.querySelector(".oval")
-
-    
-    let count = todos.length
-
-    if (p.classList.contains("completed")){
-        count--
-        console.log(count);
-    }else {
-        count++
-        console.log(count);
-        
+function completedTask(taskId) {
+    const task = todos.find(task => task.id === taskId);
+    if (task) {
+        task.completed = !task.completed;
+        const currentFilter = activeCategory;
+        updateDOM();
+        display();
+        filterTasks(currentFilter);
     }
-    display()
-    
 }
 
+categoryFilter('all');
 
+let activeCategory = 'all';
 
+function categoryFilter(activeCategory) {
+    footer.innerHTML = `
+    <div class="panel">
+    <h1 class="${activeCategory === 'all' ? "activatedCategory" : ""}" id="allCategory" onclick="filterTasks('all')">All</h1>
+    <h1 class="${activeCategory === 'active' ? "activatedCategory" : ""}" id="activeCategory" onclick="filterTasks('active')">Active</h1>
+    <h1 class="${activeCategory === 'completed' ? "activatedCategory" : ""}" id="completedCategory" onclick="filterTasks('completed')">Completed</h1>
+    </div>`;
+}
 
-// const inpt = document.querySelector("input");
-// const specs = document.querySelector(".specs");
-// const section = document.querySelector("section");
-// const main = document.querySelector("main");
+function filterTasks(taskCategory) {
+    todos.forEach(item => {
+        const taskElement = document.getElementById(item.id);
+        taskElement.classList.remove('hidden');
+        switch (taskCategory) {
+            case 'active':
+                if (item.completed) {
+                    taskElement.classList.add('hidden');
+                }
+                break;
+            case 'completed':
+                if (!item.completed) {
+                    taskElement.classList.add('hidden');
+                }
+                break;
+            default:
+                break;
+        }
+    });
 
-// let todos = [];
-
-// inpt.addEventListener("keydown", (e) => {
-//     if (e.key === "Enter") {
-//         updatePage();
-//         inpt.value = "";
-//     }
-// });
-
-// function updatePage() {
-//     if (!inpt.value.trim() == "") {
-//         let task = {
-//             title: inpt.value,
-//             id: Math.random(),
-//             completed: false
-//         };
-//         todos.push(task);
-//         main.classList.add("active");
-//         updateDOM();
-//         display();
-//     }
-// }
-
-// function display() {
-//     let count = todos.filter(task => !task.completed).length;
-//     specs.innerHTML = `
-//         <h3>${count} items left</h3>
-//         <h3>Clear Tasks</h3>`;
-//     if (count === 0) {
-//         main.classList.remove("active");
-//     }
-// }
-
-// function deleteTask(taskId) {
-//     todos = todos.filter(task => task.id !== taskId);
-//     updateDOM();
-//     display();
-// }
-
-// function updateDOM() {
-//     section.innerHTML = "";
-//     todos.forEach(task => {
-//         let taskElement = document.createElement("div");
-//         taskElement.classList.add("box");
-//         taskElement.innerHTML = `
-//         <div class="task">
-//         <div class="left">
-//                     <img  onclick="completed(${task.id})" class="oval" src="./assets/mobile/oval.svg">
-//                     <p id = "${task.id}" class="${task.completed ? 'completed' : ''}">${task.title}</p>
-//                 </div>
-//                 <img onclick="deleteTask(${task.id})" class="delete" src="./assets/mobile/X.svg">
-//             </div>
-//             <div class="line"></div>`;
-//         section.append(taskElement);
-//     });
-// }
-
-// function completed(taskId) {
-//     const task = todos.find(task => task.id == taskId);
-//     if (task) {
-//         task.completed = !task.completed;
-//     }
-//     display();
-// }
+    activeCategory = taskCategory;
+    categoryFilter(activeCategory);
+}
